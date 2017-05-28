62d199091fa07df04db0369ad0daf5da00bff59a
kilim choi <stevn1202@gmail.com>
2017-05-28 16:14:09 -0700

    initial commit

---
+++ app/models/coin.rb
@@ -0,0 +1,2 @@
+class Coin < ActiveRecord::Base
+end
---
+++ db/migrate/20170528230647_create_coins.rb
@@ -0,0 +1,15 @@
+class CreateCoins < ActiveRecord::Migration
+  def change
+    create_table :coins do |t|
+
+      t.timestamps null: false
+      t.string :symbol
+      t.string :description 
+      t.string :website 
+      t.text :what_is_special_about_it 
+      t.string :where_to_buy
+      t.string :consensus_mechanism 
+      t.string :category
+    end
+  end
+end
