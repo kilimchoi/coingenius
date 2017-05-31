b694e3caa90da79a5b81f22aae8412fe012fc952
kilim choi <stevn1202@gmail.com>
2017-05-30 22:00:31 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529034247) do
+ActiveRecord::Schema.define(version: 20170531042012) do
 
   create_table "coins", force: :cascade do |t|
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "symbol"
     t.string   "description"
     t.string   "website"
-    t.text     "what_is_special_about_it"
+    t.text     "pros"
     t.string   "where_to_buy"
     t.string   "consensus_mechanism"
     t.string   "category"
     t.string   "name"
+    t.text     "cons"
   end
 
   create_table "coins_exchanges", id: false, force: :cascade do |t|
@@ -39,20 +40,19 @@ ActiveRecord::Schema.define(version: 20170529034247) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "username",               default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "username",               default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.boolean  "is_admin",               default: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
