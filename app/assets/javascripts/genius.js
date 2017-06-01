7eb4593926c69589399664a045702c868631d477
Matthew Hui <mh@berkeley.edu>
2017-06-01 16:03:15 -0700

    more

--- app/assets/javascripts/genius.js
+++ app/assets/javascripts/genius.js
@@ -2,4 +2,27 @@ $(document).on('turbolinks:load', function() {
   // $("#transaction_coin_id").select2({
   //   theme: "bootstrap"
   // });
+  var ctx = $("#portfolioChart");
+  
+  var attrData = ctx.data()
+
+  var data        = attrData.dataset        ? eval(attrData.dataset) : {}
+  var dataOptions = attrData.datasetOptions ? eval('(' + attrData.datasetOptions + ')') : {}
+  var labels      = attrData.labels         ? eval(attrData.labels) : {}
+  var options     = attrData.options        ? eval('(' + attrData.options + ')') : {}
+
+  var datasets = $.extend({
+    data: data,
+    borderWidth: 2,
+    hoverBorderColor: 'transparent'
+  }, dataOptions)
+
+  var portfolioChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      datasets: [ datasets ],
+      labels: labels
+    },
+    options: options
+  });
 });
