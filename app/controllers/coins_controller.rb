f0fc168d506e676f018760f8e8a73795c7020f75
kilim choi <stevn1202@gmail.com>
2017-06-16 00:20:24 -0700

    remove portfolio and fix coins view

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -9,8 +9,8 @@ class CoinsController < ApplicationController
     api_coins = JSON.parse(response.body).sort_by{ |hash| hash['mktcap'].to_f }.reverse.first(100)
     api_coins.each do |api_coin|
       api_coin = api_coin.with_indifferent_access
-      coin = Coin.where(symbol: api_coin[:short]).first
-      next if coin.nil?
+      coin = Coin.where(symbol: api_coin[:short]).first || Coin.create(symbol: api_coin[:short], name: api_coin[:long])
+      next if !coin.pros
       coin.price = api_coin[:price]
       coin.percent_change = api_coin[:perc]
       coin.market_cap = api_coin[:mktcap]
