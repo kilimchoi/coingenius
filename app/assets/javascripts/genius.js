158ca4912bef6358ce436e4e92945211ce8fae52
kilim choi <stevn1202@gmail.com>
2017-06-16 00:15:29 -0700

    add

--- app/assets/javascripts/genius.js
+++ app/assets/javascripts/genius.js
@@ -1,347 +1,65 @@
-$(document).on('turbolinks:load', function() {
-  // $("#transaction_coin_id").select2({
-  //   theme: "bootstrap"
-  // });
-  var ctx  = $("#portfolioChart");
-
-  if (ctx.length) {
-    // var attrData = ctx.data()
-
-    // var data        = attrData.dataset        ? eval(attrData.dataset) : {}
-    // var dataOptions = attrData.datasetOptions ? eval('(' + attrData.datasetOptions + ')') : {}
-    // var labels      = attrData.labels         ? eval(attrData.labels) : {}
-    // var options     = attrData.options        ? eval('(' + attrData.options + ')') : {}
-
-    // var datasets = $.extend({
-    //   data: data,
-    //   borderWidth: 2,
-    //   hoverBorderColor: 'transparent'
-    // }, dataOptions)
-
-    // var portfolioChart = new Chart(ctx, {
-    //   type: 'doughnut',
-    //   data: {
-    //     datasets: [ datasets ],
-    //     labels: labels
-    //   },
-    //   options: options
-    // });
+var isDate = function(value) {
+  var dateFormat;
+  if (toString.call(value) === '[object Date]') {
+    return true;
   }
+  if (typeof value.replace === 'function') {
+    value.replace(/^\s+|\s+$/gm, '');
+  }
+  dateFormat = /(^\d{1,4}[\.|\\/|-]\d{1,2}[\.|\\/|-]\d{1,4})(\s*(?:0?[1-9]:[0-5]|1(?=[012])\d:[0-5])\d\s*[ap]m)?$/;
+  return dateFormat.test(value);
+}
 
-  $(function () {
-
-    var Charts = {
-
-      _HYPHY_REGEX: /-([a-z])/g,
-
-      _cleanAttr: function (obj) {
-        delete obj["chart"]
-        delete obj["datasets"]
-        delete obj["datasetsOptions"]
-        delete obj["labels"]
-        delete obj["options"]
-      },
-
-      // doughnut: function (element) {
-      //   var attrData = $.extend({}, $(element).data())
-
-      //   var data        = attrData.dataset        ? eval(attrData.dataset) : {}
-      //   var dataOptions = attrData.datasetOptions ? eval('(' + attrData.datasetOptions + ')') : {}
-      //   var labels      = attrData.labels         ? eval(attrData.labels) : {}
-      //   var options     = attrData.options        ? eval('(' + attrData.options + ')') : {}
-
-      //   Charts._cleanAttr(attrData)
-
-      //   var datasets = $.extend({
-      //     data: data,
-      //     borderWidth: 2,
-      //     hoverBorderColor: 'transparent'
-      //   }, dataOptions)
-
-      //   var options = $.extend({
-      //     cutoutPercentage: 80,
-      //     legend: {
-      //       display: false
-      //     },
-      //     animation: {
-      //       animateRotate: false,
-      //       duration: 0
-      //     }
-      //   }, options)
-
-      //   new Chart(element.getContext('2d'), {
-      //       type:"doughnut",
-      //       data: {
-      //         datasets: [ datasets ],
-      //         labels: labels
-      //       },
-      //       options: options
-      //   })
-      // },
-
-      // 'spark-line': function (element) {
-      //   var attrData = $.extend({}, $(element).data())
-      //   //console.log('dataset is ', eval(attrData.dataset));
-      //   var data           = attrData.dataset ? eval(attrData.dataset) : []
-      //   var datasetOptions = attrData.datasetOptions ? eval(attrData.datasetOptions) : []
-      //   var labels         = attrData.weeklylabels ? eval(attrData.weeklylabels) : {}
-      //   var options     = attrData.options        ? eval('(' + attrData.options + ')') : {}
-      //   var isDark         = !!attrData.dark
-
-      //   var data = {
-      //     labels   : labels,
-      //     datasets : data.map(function (set, i) {
-      //       return $.extend({
-      //         data: set,
-      //         fill: true,
-      //         backgroundColor: 'rgba(255,255,255,.3)',
-      //         borderColor: '#fff',
-      //         pointBorderColor: '#fff',
-      //         lineTension : 0.25,
-      //         pointRadius: 0,
-      //         pointHoverRadius: 0,
-      //         pointHitRadius: 20
-      //       }, datasetOptions[i])
-      //     })
-      //   }
-
-      //   Charts._cleanAttr(attrData)
-
-      //   var options = $.extend({
-      //     animation: {
-      //       duration: 0
-      //     },
-      //     legend: {
-      //       display: false
-      //     },
-      //     scales: {
-      //       yAxes: [{
-      //         gridLines: {
-      //           color: isDark ? 'rgba(255,255,255,.05)' : 'rgba(0,0,0,.05)',
-      //           zeroLineColor: isDark ? 'rgba(255,255,255,.05)' : 'rgba(0,0,0,.05)',
-      //           drawBorder: false
-      //         },
-      //         ticks: {
-      //           beginAtZero: false,
-      //           fixedStepSize: 1000,
-      //           fontColor: isDark ? '#a2a2a2' : 'rgba(0,0,0,.4)',
-      //           fontSize: 14
-      //         }
-      //       }],
-      //       xAxes: [{
-      //         gridLines: {
-      //           display: false
-      //         },
-      //         ticks: {
-      //           fontColor: isDark ? '#a2a2a2' : 'rgba(0,0,0,.4)',
-      //           fontSize: 14
-      //         }
-      //       }]
-      //     },
-      //     tooltips: {
-      //       enabled: true,
-      //       bodyFontSize: 14,
-      //       callbacks: {
-      //         title: function () { return "" },
-      //         labelColor: function () {
-      //           return {
-      //             backgroundColor: '#42a5f5',
-      //             borderColor: '#42a5f5'
-      //           }
-      //         }
-      //       }
-      //     }
-      //   }, options)
-
-      //   var lineChart = new Chart(element.getContext('2d'), {
-      //       type: 'line',
-      //       data: data,
-      //       options: options
-      //   })
-      // },
-
-      // line: function (element) {
-      //   var attrData = $.extend({}, $(element).data())
-
-      //   var data           = attrData.dataset        ? eval(attrData.dataset) : []
-      //   var datasetOptions = attrData.datasetOptions ? eval(attrData.datasetOptions) : []
-      //   var labels         = attrData.labels         ? eval(attrData.labels) : {}
-      //   var options        = attrData.options        ? eval('(' + attrData.options + ')') : {}
-      //   var isDark         = !!attrData.dark
-
-      //   var data = {
-      //     labels   : labels,
-      //     datasets : data.map(function (set, i) {
-      //       return $.extend({
-      //         data: set,
-      //         fill: true,
-      //         backgroundColor: isDark ? 'rgba(28,168,221,.03)' : 'rgba(66,165,245,.2)',
-      //         borderColor: '#42a5f5',
-      //         pointBorderColor: '#fff',
-      //         lineTension : 0.25,
-      //         pointRadius: 0,
-      //         pointHoverRadius: 0,
-      //         pointHitRadius: 20
-      //       }, datasetOptions[i])
-      //     })
-      //   }
-
-      //   Charts._cleanAttr(attrData)
-
-      //   var options = $.extend({
-      //     maintainAspectRatio: false,
-      //     animation: {
-      //       duration: 0
-      //     },
-      //     legend: {
-      //       display: false
-      //     },
-      //     scales: {
-      //       yAxes: [{
-      //         gridLines: {
-      //           color: isDark ? 'rgba(255,255,255,.05)' : 'rgba(0,0,0,.05)',
-      //           zeroLineColor: isDark ? 'rgba(255,255,255,.05)' : 'rgba(0,0,0,.05)',
-      //           drawBorder: false
-      //         },
-      //         ticks: {
-      //           beginAtZero: false,
-      //           fixedStepSize: 1000,
-      //           fontColor: isDark ? '#a2a2a2' : 'rgba(0,0,0,.4)',
-      //           fontSize: 14
-      //         }
-      //       }],
-      //       xAxes: [{
-      //         gridLines: {
-      //           display: false
-      //         },
-      //         ticks: {
-      //           fontColor: isDark ? '#a2a2a2' : 'rgba(0,0,0,.4)',
-      //           fontSize: 14
-      //         }
-      //       }]
-      //     },
-      //     tooltips: {
-      //       enabled: true,
-      //       bodyFontSize: 14,
-      //       callbacks: {
-      //         title: function () { return "" },
-      //         labelColor: function () {
-      //           return {
-      //             backgroundColor: '#42a5f5',
-      //             borderColor: '#42a5f5'
-      //           }
-      //         }
-      //       }
-      //     }
-      //   }, options)
-
-      //   new Chart(element.getContext('2d'), {
-      //       type: 'line',
-      //       data: data,
-      //       options: options
-      //   })
-      // },
-
-      // bar: function (element) {
-      //   var attrData = $.extend({}, $(element).data())
-
-      //   var data           = attrData.dataset        ? eval(attrData.dataset) : []
-      //   var datasetOptions = attrData.datasetOptions ? eval(attrData.datasetOptions) : []
-      //   var labels         = attrData.labels         ? eval(attrData.labels) : {}
-      //   var options        = attrData.options        ? eval('(' + attrData.options + ')') : {}
-      //   var isDark         = !!attrData.dark
-
-      //   var data = {
-      //     labels   : labels,
-      //     datasets : data.map(function (set, i) {
-      //       return $.extend({
-      //         data: set,
-      //         fill: true,
-      //         backgroundColor: (i % 2 ? '#42a5f5' : '#1bc98e'),
-      //         borderColor: 'transparent'
-      //       }, datasetOptions[i])
-      //     })
-      //   }
-
-      //   Charts._cleanAttr(attrData)
-
-      //   var options = $.extend({
-      //     maintainAspectRatio: false,
-      //     animation: {
-      //       duration: 0
-      //     },
-      //     legend: {
-      //       display: false
-      //     },
-      //     scales: {
-      //       yAxes: [{
-      //         gridLines: {
-      //           color: isDark ? 'rgba(255,255,255,.05)' : 'rgba(0,0,0,.05)',
-      //           zeroLineColor: isDark ? 'rgba(255,255,255,.05)' : 'rgba(0,0,0,.05)',
-      //           drawBorder: false
-      //         },
-      //         ticks: {
-      //           fixedStepSize: 25,
-      //           fontColor: isDark ? '#a2a2a2' : 'rgba(0,0,0,.4)',
-      //           fontSize: 14
-      //         }
-      //       }],
-      //       xAxes: [{
-      //         gridLines: {
-      //           display: false
-      //         },
-      //         ticks: {
-      //           fontColor: isDark ? '#a2a2a2' : 'rgba(0,0,0,.4)',
-      //           fontSize: 14
-      //         }
-      //       }]
-      //     },
-      //     tooltips: {
-      //       enabled: true,
-      //       bodyFontSize: 14
-      //     }
-      //   }, options)
-
-      //   new Chart(element.getContext('2d'), {
-      //       type: 'bar',
-      //       data: data,
-      //       options: options
-      //   })
-      // }
+var convertFromTimeStampToDate = function(labels) { 
+  for (var i = 0; i < labels.length; i++) {
+    if (!isDate(labels[i])) {
+      var date = new Date(labels[i])
+      var hours = date.getHours();
+      var mins = date.getMinutes();
+      mins = mins < 10 ? "0" + mins : mins
+      var ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours ? hours : 12; 
+      labels[i] = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear().toString().substr(-2) + " " + hours + ":" + mins + ampm;
     }
+  }
+  return labels;
+}
+$(document).on('turbolinks:load', function() {
+  $(function () {
     var linechart;
+
     $('.nav-link').on('click', function() {
         $('.nav-link').removeClass('active');
         $(this).addClass('active');
         if($(this).attr('id') == "daily") {
           $('[data-chart]').each(function () {
-            window.lineChart.data.labels = $(this).data().dailylabels;
-            window.lineChart.data.datasets.forEach((dataset) => {
-              dataset.data.pop();
-            });
-            window.lineChart.data.datasets.forEach((dataset) => {
-              dataset.data.push($(this).data().dataset);
-            });
+            var labels = $(this).data().dailylabels;
+            window.lineChart.data.labels = convertFromTimeStampToDate(labels);
+            window.lineChart.data.datasets[0].data = $(this).data().dataset[0]
             window.lineChart.update();
           });
         }
         if($(this).attr('id') == "weekly") {
           $('[data-chart]').each(function () {
-            window.lineChart.data.labels = $(this).data().weeklylabels;
+            var labels = $(this).data().weeklylabels;
+            window.lineChart.data.labels = convertFromTimeStampToDate(labels);
             window.lineChart.data.datasets[0].data = $(this).data().weeklydataset[0]
             window.lineChart.update();
           });
         }
         if($(this).attr('id') == "monthly") {
           $('[data-chart]').each(function () {
-            window.lineChart.data.labels = $(this).data().monthlylabels;
+            var labels = $(this).data().monthlylabels;
+            window.lineChart.data.labels = convertFromTimeStampToDate(labels);
             window.lineChart.data.datasets[0].data = $(this).data().monthlydataset[0]
             window.lineChart.update();
           });
         }
         if($(this).attr('id') == "yearly") {
           $('[data-chart]').each(function () {
-            window.lineChart.data.labels = $(this).data().yearlylabels;
+            var labels = $(this).data().yearlylabels;
+            window.lineChart.data.labels = convertFromTimeStampToDate(labels);
             window.lineChart.data.datasets[0].data = $(this).data().yearlydataset[0]
             window.lineChart.update();
           });
@@ -354,13 +72,14 @@ $(document).on('turbolinks:load', function() {
           if ($(this).is(':visible') && !$(this).hasClass('js-chart-drawn')) {
             var element = $(this);
             var attrData = $.extend({}, element.data())
-            //console.log('dataset is ', eval(attrData.dataset));
             var data           = attrData.dataset ? eval(attrData.dataset) : []
             var datasetOptions = attrData.datasetOptions ? eval(attrData.datasetOptions) : []
             var labels         = attrData.dailylabels ? eval(attrData.dailylabels) : {}
             var options     = attrData.options        ? eval('(' + attrData.options + ')') : {}
             var isDark         = !!attrData.dark
 
+            labels = convertFromTimeStampToDate(labels);
+            
             var data = {
               labels   : labels,
               datasets : data.map(function (set, i) {
@@ -377,8 +96,6 @@ $(document).on('turbolinks:load', function() {
                 }, datasetOptions[i])
               })
             }
-
-            Charts._cleanAttr(attrData)
 
             var options = $.extend({
               animation: {
@@ -398,29 +115,22 @@ $(document).on('turbolinks:load', function() {
                     beginAtZero: false,
                     fontColor: isDark ? '#a2a2a2' : 'rgba(0,0,0,.4)',
                     fontSize: 14, 
-                    stepSize: 10000
-                  }
+                    stacked: false,
+                    steps: 20,
+                  }, 
                 }],
                 xAxes: [{
-                  gridLines: {
-                    display: false
-                  },
-                  ticks: {
-                    fontColor: isDark ? '#a2a2a2' : 'rgba(0,0,0,.4)',
-                    fontSize: 14
-                  }
+                  display: true
                 }]
               },
               tooltips: {
                 enabled: true,
+                intersect: true,
                 bodyFontSize: 14,
                 callbacks: {
-                  title: function () { return "" },
-                  labelColor: function () {
-                    return {
-                      backgroundColor: '#42a5f5',
-                      borderColor: '#42a5f5'
-                    }
+                  title: function () { return "Total" },
+                  label: function(tooltipItem, data) {
+                    return tooltipItem.xLabel + ": $" + tooltipItem.yLabel.toFixed(2);
                   }
                 }
               }
