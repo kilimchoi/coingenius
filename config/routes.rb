57160c6e63c07bd8e7dd93fe25bea17325352ca9
kilim choi <stevn1202@gmail.com>
2017-07-20 21:49:53 -0700

    fix a nil bug for percentage change and add privacy policy and terms of use

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get '/terms', to: 'static_pages#terms', :as => :terms
+  get '/privacy', to: 'static_pages#privacy', :as => :privacy
   # You can have the root of your site routed with "root"
   root to: redirect('/coins')
   require 'sidekiq/web'
