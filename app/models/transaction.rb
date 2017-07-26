0a4c53f85795df603f52b0a7625e39a5bc0e45c5
Timur Khafizov <hangmany@gmail.com>
2017-07-26 20:33:44 +0200

    Small refactor for `Transaction`

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,16 +2,12 @@ class Transaction < ActiveRecord::Base
   belongs_to :user
   belongs_to :coin
 
-  enum transaction_type: [ :bought, :sold ]
+  enum transaction_type: %i(bought sold)
 
   validates :transaction_type, presence: true
   validates :amount, presence: true
 
   def description
-    desc = ""
-    desc += transaction_type.titleize
-    desc += " "
-    desc += " #{coin.name}"  
+    [transaction_type.titleize, coin.name].join(" ")
   end
-  
 end
