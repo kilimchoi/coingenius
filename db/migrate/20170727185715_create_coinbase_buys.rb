f002cb700c6bfddf4ad04d5e881cd1fd9c2b56af
Timur Khafizov <hangmany@gmail.com>
2017-07-27 21:13:12 +0200

    Create `Coinbase::Buy`

---
+++ db/migrate/20170727185715_create_coinbase_buys.rb
@@ -0,0 +1,10 @@
+class CreateCoinbaseBuys < ActiveRecord::Migration
+  def change
+    create_table :coinbase_buys do |t|
+      t.belongs_to :transaction, index: true, foreign_key: true
+
+      t.jsonb :raw_data, {}
+      t.string :uuid, null: false
+    end
+  end
+end
