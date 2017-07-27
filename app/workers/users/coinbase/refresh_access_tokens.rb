ac6143937cd6fb37fea76daa6cb6dad698ce6487
Timur Khafizov <hangmany@gmail.com>
2017-07-27 21:51:37 +0200

    Add worker to refresh Coinbase access and refresh tokens

---
+++ app/workers/users/coinbase/refresh_access_tokens.rb
@@ -0,0 +1,26 @@
+module Users
+  module Coinbase
+    class RefreshAccessTokens
+      include Sidekiq::Worker
+
+      def perform
+        Identity.where(provider: :coinbase).find_each do |identity|
+          # Init client with an existing refresh token
+          client = ::Coinbase::Wallet::OAuthClient.new(
+            access_token: identity.access_token,
+            refresh_token: identity.refresh_token
+          )
+
+          # Refresh an access token
+          client.refresh!
+
+          # Save new tokens
+          identity.update!(
+            access_token: client.access_token,
+            refresh_token: client.refresh_token
+          )
+        end
+      end
+    end
+  end
+end
