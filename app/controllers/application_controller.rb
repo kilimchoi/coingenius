abbc833f88f8313d68cd1c1724d8505e7741adb5
kilim choi <stevn1202@gmail.com>
2017-08-01 21:13:52 -0700

    redirect to previous page after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :store_current_location, :unless => :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -10,5 +10,15 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    session["user_return_to"] || root_path
+  end
+
+  def store_current_location
+    store_location_for(:user, request.url)
   end
 end
