f63c6c241e35eb6c756ebcf5eb48440630073108
kilim choi <stevn1202@gmail.com>
2017-08-05 18:32:07 -0700

    add workers for updating price every 5 minutes and store historical data

---
+++ app/workers/coins/sync_daily_price_for_coin_worker.rb
@@ -0,0 +1,21 @@
+module Coins
+  class SyncDailyPriceForCoinWorker
+    include Sidekiq::Worker
+    sidekiq_options retry: 5
+
+    def perform(coin_id)
+      coin = Coin.find_by(id: coin_id)
+      response = HTTParty.get("https://min-api.cryptocompare.com/data/price?fsym=#{coin.symbol.upcase}&tsyms=USD")
+      price = JSON.parse(response.body).with_indifferent_access["USD"]
+      key = price_today_key(coin.symbol)
+      $redis.set(key, price)
+      $redis.expire(key, 5.minutes)
+    end
+    
+    private
+
+    def price_today_key(symbol)
+      "#{symbol}_price_today"
+    end
+  end
+end
