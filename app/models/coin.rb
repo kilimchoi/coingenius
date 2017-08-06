f6eac9c49b60a3fcc57c1e1ba876883f097b4361
kilim choi <stevn1202@gmail.com>
2017-08-05 18:33:28 -0700

    more

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -7,25 +7,11 @@ class Coin < ActiveRecord::Base
   def price_history(days=7)
     a = []
     day_array = Coin.days_ago(days)
-    if $redis.get(price_history_key(day_array.last)) && $redis.get(price_history_key(day_array.first))
-      day_array.each do |day_timestamp|
-        a << $redis.get(price_history_key(day_timestamp)).to_f
-      end
-    else
-      if self.symbol == "MIOTA"
-        response = HTTParty.get("https://min-api.cryptocompare.com/data/histoday?fsym=IOT&tsym=USD&limit=#{days-1}&e=CCCAGG")
-      else
-        response = HTTParty.get("https://min-api.cryptocompare.com/data/histoday?fsym=#{symbol.upcase}&tsym=USD&limit=#{days-1}&e=CCCAGG")
-      end
-      prices = JSON.parse(response.body).with_indifferent_access["Data"]
-      prices.each do |price|
-        timestamp = Time.at(price[:time])
-        key = price_history_key(timestamp.beginning_of_day.to_i)
-        $redis.set(key, price[:close])
-        $redis.expire(key, 1.day.to_i)
-        a << price[:close]
-      end
+    day_array.each do |timestamp|
+      priceHistory = PriceHistory.find_by_timestamp_and_coin_id(timestamp, id) 
+      a << priceHistory.coin_price
     end
+    a[-1] = $redis.get("#{symbol}_price_today")
     a
   end
 
@@ -36,7 +22,7 @@ class Coin < ActiveRecord::Base
   def self.days_ago(days=7)
      a = []
      (days-1).downto(0) do |i|
-       a << (Date.today - i.days).to_time.to_i
+       a << (Date.today - i.days).to_time(:utc)
      end
      a
   end
