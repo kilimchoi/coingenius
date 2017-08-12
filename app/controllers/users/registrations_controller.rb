a9d792576b43ec59e85c2aa41afd95e7983624c9
Timur Khafizov <hangmany@gmail.com>
2017-08-12 14:18:43 +0200

    Allow user to save Bittrex API key and secret

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,19 @@
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :configure_permitted_parameters
+
+    private
+
+    # Skip password confirmation on user account update
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(
+        :account_update,
+        keys: %i(bittrex_api_key bittrex_api_secret username)
+      )
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers:
+    { omniauth_callbacks: "users/omniauth_callbacks",
+      registrations: "users/registrations" }
 
   # Static pages
   get "/terms", to: "static_pages#terms", :as => :terms
