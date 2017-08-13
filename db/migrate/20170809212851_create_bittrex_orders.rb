27ae8554334d64c05977207f6a5ccabce5d28630
Timur Khafizov <hangmany@gmail.com>
2017-08-13 20:47:55 +0200

    Rename `BittrexDeposit` to `BittrexOrder`

--- db/migrate/20170809212851_create_bittrex_orders.rb
+++ db/migrate/20170809212851_create_bittrex_orders.rb
@@ -1,6 +1,6 @@
-class CreateBittrexDeposits < ActiveRecord::Migration
+class CreateBittrexOrders < ActiveRecord::Migration
   def change
-    create_table :bittrex_deposits do |t|
+    create_table :bittrex_orders do |t|
       t.belongs_to :transaction, index: true, foreign_key: true
       t.jsonb :raw_data
       t.string :uuid
