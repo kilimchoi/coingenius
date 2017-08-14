f6a131fe2fe50b36154a1ef337e77c40a7e7c9cc
Timur Khafizov <hangmany@gmail.com>
2017-08-14 22:08:35 +0200

    Fix sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,6 +11,11 @@ module Users
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(
+        :sign_up,
+        keys: %i(username)
+      )
+
+      devise_parameter_sanitizer.permit(
         :account_update,
         keys: %i(bittrex_api_key bittrex_api_secret username)
       )
