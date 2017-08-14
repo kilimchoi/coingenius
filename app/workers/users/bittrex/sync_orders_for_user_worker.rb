0ecd8df032748e9696c2319bcb4b2514dc27fbd3
Timur Khafizov <hangmany@gmail.com>
2017-08-14 22:16:32 +0200

    Create workers for syncing Bittrex orders

---
+++ app/workers/users/bittrex/sync_orders_for_user_worker.rb
@@ -0,0 +1,17 @@
+module Users
+  module Bittrex
+    class SyncOrdersForUserWorker
+      include Sidekiq::Worker
+
+      sidekiq_options retry: 5
+
+      def perform(user_id)
+        user = User.find_by(id: user_id)
+
+        return unless user&.bittrex_api_key && user&.bittrex_api_secret
+
+        Users::Bittrex::SyncOrders.call(user: user)
+      end
+    end
+  end
+end
