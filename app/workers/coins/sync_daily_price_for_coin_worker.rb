f0a8802d416276a9608b43fb949cedf10ec88c47
kilim choi <stevn1202@gmail.com>
2017-08-20 11:07:53 -0700

    remove retry

--- app/workers/coins/sync_daily_price_for_coin_worker.rb
+++ app/workers/coins/sync_daily_price_for_coin_worker.rb
@@ -12,7 +12,6 @@ module Coins
       $redis.expire(key, 5.minutes)
     rescue StandardError => e
       logger.warn "performing Coins::SyncDailyPriceForCoinWorker again in 1 minute due to #{e.message}"
-      self.class.perform_in(1.minute, coin_id)
     end
     
     private
