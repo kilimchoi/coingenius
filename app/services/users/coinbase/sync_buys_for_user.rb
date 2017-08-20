3899278608b4fd74f438d114a11a49cacc230023
Timur Khafizov <hangmany@gmail.com>
2017-08-20 23:03:55 +0200

    Sync Coinbase sells and buys from all wallets

--- app/services/users/coinbase/sync_buys_for_user.rb
+++ app/services/users/coinbase/sync_buys_for_user.rb
@@ -6,36 +6,16 @@ module Users
       delegate :user, to: :context
 
       def call
-        buys.each do |buy_transaction|
-          # Skip uncompleted transactions
-          next if buy_transaction["status"] != "completed"
+        client.accounts.each do |account|
+          buys = client.list_buys(account.id)
 
-          # Or if we already processed this transaction
-          next if ::Coinbase::Buy.where(uuid: buy_transaction["id"]).exists?
-
-          process_transaction(buy_transaction)
+          buys.each do |buy_transaction|
+            Users::Coinbase::CreateTransactionFromBuy.call(user: user, buy: buy_transaction)
+          end
         end
       end
 
       private
-
-      def process_transaction(buy_transaction)
-        transaction = user.transactions.create(
-          amount: BigDecimal.new(buy_transaction["amount"]["amount"]),
-          coin: Coin.find_by(symbol: buy_transaction["amount"]["currency"]),
-          price: BigDecimal.new(buy_transaction["subtotal"]["amount"]) / BigDecimal(buy_transaction["amount"]["amount"]),
-          transaction_type: :bought
-        )
-
-        transaction.create_coinbase_buy!(
-          uuid: buy_transaction["id"],
-          raw_data: buy_transaction
-        )
-      end
-
-      def buys
-        client.primary_account.list_buys
-      end
 
       def client
         @client ||= ::Coinbase::Wallet::OAuthClient.new(access_token: coinbase_identity.access_token)
--- app/services/users/coinbase/sync_sells_for_user.rb
+++ app/services/users/coinbase/sync_sells_for_user.rb
@@ -6,36 +6,16 @@ module Users
       delegate :user, to: :context
 
       def call
-        sells.each do |sell_transaction|
-          # Skip uncompleted transactions
-          next if sell_transaction["status"] != "completed"
+        client.accounts.each do |account|
+          sells = client.list_sells(account.id)
 
-          # Or if we already processed this transaction
-          next if ::Coinbase::Sell.where(uuid: sell_transaction["id"]).exists?
-
-          process_transaction(sell_transaction)
+          sells.each do |sell_transaction|
+            Users::Coinbase::CreateTransactionFromSell.call(user: user, sell: sell_transaction)
+          end
         end
       end
 
       private
-
-      def process_transaction(sell_transaction)
-        transaction = user.transactions.create!(
-          amount: BigDecimal.new(sell_transaction["amount"]["amount"]),
-          coin: Coin.find_by(symbol: sell_transaction["amount"]["currency"]),
-          price: BigDecimal.new(sell_transaction["subtotal"]["amount"]) / BigDecimal.new(sell_transaction["amount"]["amount"]),
-          transaction_type: :sold
-        )
-
-        transaction.create_coinbase_sell!(
-          uuid: sell_transaction["id"],
-          raw_data: sell_transaction
-        )
-      end
-
-      def sells
-        client.primary_account.list_sells
-      end
 
       def client
         @client ||= ::Coinbase::Wallet::OAuthClient.new(access_token: coinbase_identity.access_token)
