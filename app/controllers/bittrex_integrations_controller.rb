c36aed59b0f8bbd211c38b21ccf28382e7286ed7
kilim choi <stevn1202@gmail.com>
2017-08-22 22:23:07 -0700

    add back duplicate api key

--- app/controllers/bittrex_integrations_controller.rb
+++ app/controllers/bittrex_integrations_controller.rb
@@ -6,7 +6,10 @@ class BittrexIntegrationsController < ApplicationController
   end
 
   def create
-    if user.update_attributes(bittrex_params)
+    accounts_with_same_api_key = User.where(bittrex_api_key: params["user"]["bittrex_api_key"])
+    if !accounts_with_same_api_key.empty?
+      flash[:error] = "You have already imported transactions from your Bittrex account. Please use a different Bittrex account and create a new key."
+    elsif user.update_attributes(bittrex_params)
       flash[:success] = "You successfully integrated with bittrex. We will be pulling transactions from bittrex."
       Users::Bittrex::SyncOrdersForUserWorker.perform_async(user.id)
     else
