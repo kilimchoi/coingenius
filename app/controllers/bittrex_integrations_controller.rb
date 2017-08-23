24f19d81dbe119b64741ce8ab794628e9b366757
kilim choi <stevn1202@gmail.com>
2017-08-22 22:04:38 -0700

    add is_expired to check for deleted transactions

--- app/controllers/bittrex_integrations_controller.rb
+++ app/controllers/bittrex_integrations_controller.rb
@@ -3,12 +3,12 @@ class BittrexIntegrationsController < ApplicationController
   expose :user, -> { current_user }
 
   def new
-    respond_with user
+    
   end
 
   def create
     if user.update_attributes(bittrex_params)
-      flash[:success] = 'You successfully integrated with bittrex. We will be pulling transactions from bittrex.'
+      flash[:success] = "You successfully integrated with bittrex. We will be pulling transactions from bittrex."
       Users::Bittrex::SyncOrdersForUserWorker.perform_async(user.id)
     else
       flash[:error] = user.errors.full_messages.join(', ')
