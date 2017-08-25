ea4c7bddee7ab205e793e84ed19647239a311ecc
kilim choi <stevn1202@gmail.com>
2017-08-24 20:24:30 -0700

    check for nil coinbase client to avoid errors

--- app/services/users/coinbase/sync_buys_for_user.rb
+++ app/services/users/coinbase/sync_buys_for_user.rb
@@ -5,20 +5,23 @@ module Users
 
       delegate :user, to: :context
 
-      def call
-        client.accounts.each do |account|
-          buys = client.list_buys(account.id)
+      def call 
+        unless client.nil?
+          client.accounts.each do |account|
+            buys = client.list_buys(account.id)
 
-          buys.each do |buy_transaction|
-            Users::Coinbase::CreateTransactionFromBuy.call(user: user, buy: buy_transaction)
+            buys.each do |buy_transaction|
+              Users::Coinbase::CreateTransactionFromBuy.call(user: user, buy: buy_transaction)
+            end
           end
         end
+        nil
       end
 
       private
 
       def client
-        @client ||= ::Coinbase::Wallet::OAuthClient.new(access_token: coinbase_identity.access_token)
+        @client ||= ::Coinbase::Wallet::OAuthClient.new(access_token: coinbase_identity.access_token) unless coinbase_identity.nil?
       end
 
       def coinbase_identity
--- app/services/users/coinbase/sync_sells_for_user.rb
+++ app/services/users/coinbase/sync_sells_for_user.rb
@@ -6,19 +6,22 @@ module Users
       delegate :user, to: :context
 
       def call
-        client.accounts.each do |account|
-          sells = client.list_sells(account.id)
+        unless client
+          client.accounts.each do |account|
+            sells = client.list_sells(account.id)
 
-          sells.each do |sell_transaction|
-            Users::Coinbase::CreateTransactionFromSell.call(user: user, sell: sell_transaction)
+            sells.each do |sell_transaction|
+              Users::Coinbase::CreateTransactionFromSell.call(user: user, sell: sell_transaction)
+            end
           end
         end
+        nil
       end
 
       private
 
       def client
-        @client ||= ::Coinbase::Wallet::OAuthClient.new(access_token: coinbase_identity.access_token)
+        @client ||= ::Coinbase::Wallet::OAuthClient.new(access_token: coinbase_identity.access_token) unless coinbase_identity.nil?
       end
 
       def coinbase_identity
