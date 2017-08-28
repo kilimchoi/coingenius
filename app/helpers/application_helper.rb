6606c2f2c2fab59f0a44714fd540399a6e0b15ae
kilim choi <stevn1202@gmail.com>
2017-08-27 23:32:47 -0700

    remove

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,10 +49,4 @@ module ApplicationHelper
   def base_url(url)
     URI.parse(url).host
   end
-
-  def show_svg(path)
-    File.open("app/assets/images/#{path}", "rb") do |file|
-      raw file.read
-    end
-  end
 end
