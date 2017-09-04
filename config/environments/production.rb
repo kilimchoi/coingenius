61971c3aa06dd3ab1c71eacbd4c211b752dba292
kilim choi <stevn1202@gmail.com>
2017-09-03 20:37:10 -0700

    remove runtime errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.raise_runtime_errors = true
+  # config.assets.raise_runtime_errors = true
 end
