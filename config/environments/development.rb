5e5eae95b0403d923cb38efd97d8d245bd9f6240
Timur Khafizov <hangmany@gmail.com>
2017-09-08 12:43:05 +0200

    Use configured full host to prevent hardcoding a URL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.full_host = "coingenius.dev"
+  config.full_url = "http://#{config.full_host}"
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
