6cc9b7a65af52f5c8418f4d1e7010007ac6c6ea8
kilim choi <stevn1202@gmail.com>
2017-09-09 10:06:17 -0700

    log failing messages

--- app/services/users/coinbase/sync_sells_for_user.rb
+++ app/services/users/coinbase/sync_sells_for_user.rb
@@ -16,7 +16,8 @@ module Users
           end
         end
         nil
-      rescue ::Coinbase::Wallet::APIError
+      rescue ::Coinbase::Wallet::APIError => e
+        logger.warn "Sync sell failed. Coinbase::Wallet::APIError: #{e.message} user email: #{user.email}"
         nil
       end
 
