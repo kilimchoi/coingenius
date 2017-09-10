082ada9f5f93db0832bc1c6fab1fbdd81622329a
kilim choi <stevn1202@gmail.com>
2017-09-09 18:44:44 -0700

    whitelist access

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -1,8 +1,5 @@
 class AdminConstraint
   def matches?(request)
-    puts 'request is ', request.session.inspect
-    return false unless request.session[:current_user_id]
-    user = Sys::User.find request.session[:current_user_id]
-    user && user.admin?
+    puts 'session:', request.session.inspect
   end
 end
