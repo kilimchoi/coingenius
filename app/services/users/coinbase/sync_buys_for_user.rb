9f7120f597b9927be8459d9fdb890b703fb02526
kilim choi <stevn1202@gmail.com>
2017-09-10 08:28:16 -0700

    add rails to fix logger

--- app/services/users/coinbase/sync_buys_for_user.rb
+++ app/services/users/coinbase/sync_buys_for_user.rb
@@ -17,7 +17,7 @@ module Users
         end
         nil
       rescue ::Coinbase::Wallet::APIError => e
-        logger.warn "Sync buy failed. Coinbase::Wallet::APIError: #{e.message} user email: #{user.email}"
+        Rails.logger.warn "Sync buy failed. Coinbase::Wallet::APIError: #{e.message} user email: #{user.email}"
         nil
       end
 
