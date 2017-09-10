6439630397154b68b6780e435120849f7a948110
Timur Khafizov <hangmany@gmail.com>
2017-09-10 11:59:21 +0200

    Add service to process Bittrex CSV file

--- app/services/users/bittrex/process_orders_history.rb
+++ app/services/users/bittrex/process_orders_history.rb
@@ -3,21 +3,37 @@ module Users
     class ProcessOrdersHistory
       include Interactor
 
-      delegate :csv_file, to: :context
+      MAPPED_HEADERS = [
+        "Closed", "TimeStamp", "Exchange", "OrderType", "Bid/Ask", "Units Filled", "Quantity", "PricePerUnit", "Cost / Proceeds"
+      ]
+
+      delegate :csv_file_path, :user, to: :context
 
       def call
-        orders.each do |order|
-          # 1. Check if we already created an order.
-          # 2. Process order if not created yet
+        CSV.foreach(csv_file_path, headers: MAPPED_HEADERS).each_with_index do |order_row, index|
+          # Skip initial headers row
+          next if index.zero?
+
+          # Add missing info to data hash
+          order_row.tap do |order_data|
+            order_data["Closed"] = Time.strptime(order_data["Closed"], "%m/%d/%Y %l:%M:%S %p").to_s
+            order_data["OrderType"].sub!(" ", "_").upcase!
+            order_data["QuantityRemaining"] = (BigDecimal.new(order_data["Quantity"], 12) - BigDecimal.new(order_data["Units Filled"], 12)).to_f
+            order_data["TimeStamp"] = Time.strptime(order_data["TimeStamp"], "%m/%d/%Y %l:%M:%S %p").to_s
+          end
+
+          # Check if we already created an order by checking via
+          #   [transaction type, closed date, market]
+          next if BittrexOrder
+            .where("raw_data->>'OrderType' = ?", order_row["OrderType"])
+            .where(closed_at: Time.parse(order_row["Closed"]))
+            .where("raw_data->>'Exchange' = ?", order_row["Exchange"])
+            .exists?
+
+          order = ::Bittrex::Order.new(order_row.to_h)
+          BittrexOrders::CreateFromOrder.call(order: order, user: user)
         end
       end
-
-      private
-
-      def orders
-
-      end
-
     end
   end
 end
