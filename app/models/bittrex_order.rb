63292ca5ad8d1aead27a9b9551225ad250fabcdf
Timur Khafizov <hangmany@gmail.com>
2017-09-11 21:05:37 +0200

    Delete `Transaction` along with `BittrexOrder`

--- app/models/bittrex_order.rb
+++ app/models/bittrex_order.rb
@@ -1,5 +1,5 @@
 class BittrexOrder < ActiveRecord::Base
-  belongs_to :user_transaction, class_name: "Transaction", foreign_key: :transaction_id
+  belongs_to :user_transaction, class_name: "Transaction", foreign_key: :transaction_id, dependent: :destroy
   belongs_to :bittrex_orders_history_import
 
   validates :executed_at, presence: true
