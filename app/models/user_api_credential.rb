09f0906bc1225af6b65e6d858b9d383bd08edfd8
Timur Khafizov <hangmany@gmail.com>
2017-09-19 20:43:42 +0200

    Rename column `user_api_credential.api_key` to `user_api_credential.key` and set index on it

--- app/models/user_api_credential.rb
+++ app/models/user_api_credential.rb
@@ -1,21 +1,17 @@
 class UserApiCredential < ApplicationRecord
   belongs_to :user
 
-  validates :api_key, :key, :secret, presence: true
+  validates :key, :secret, presence: true
 
   before_validation :assign_key_and_secret, on: :create
 
-  # Encrypt key and secret with a special key which should NEVER change
-  with_options(key: ENV["USER_API_CREDENTIAL_256_BIT_KEY"], if: Rails.env.production?) do
-    attr_encrypted :key
-    attr_encrypted :secret
-  end
+  # Encrypt API secret with a special key which should NEVER change
+  attr_encrypted :secret, key: ENV["USER_API_CREDENTIAL_256_BIT_KEY"], if: Rails.env.production?
 
   private
 
   def assign_key_and_secret
-    self.api_key = SecureRandom.hex(24)
-    self.key = api_key
+    self.key = SecureRandom.hex(24)
     self.secret = SecureRandom.hex(64)
   end
 end
