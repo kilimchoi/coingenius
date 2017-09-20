7cdad5cbc09f66c24884a3f1a68131d234ecb184
Timur Khafizov <hangmany@gmail.com>
2017-09-20 22:02:38 +0200

    Do not prevent user to update his own Bittrex API key

--- app/controllers/bittrex_integrations_controller.rb
+++ app/controllers/bittrex_integrations_controller.rb
@@ -3,22 +3,20 @@ class BittrexIntegrationsController < ApplicationController
 
   before_action :strip_whitespaces_in_params!, only: :create
 
-  expose :user, -> {current_user}
-
   def new
   end
 
   def create
-    accounts_with_same_api_key = User.where(bittrex_api_key: bittrex_params[:bittrex_api_key])
+    accounts_with_same_api_key = User.where(bittrex_api_key: bittrex_params[:bittrex_api_key]).where.not(id: current_user.id)
 
     if accounts_with_same_api_key.any?
       flash[:error] = "You have already imported transactions from your Bittrex account using another CoinGenius account. Please use a different Bittrex account and create a new key."
-    elsif user.update(bittrex_params)
+    elsif current_user.update(bittrex_params)
       flash[:success] = "You successfully integrated with bittrex. We will be pulling transactions from bittrex."
 
-      Users::Bittrex::SyncOrdersForUserWorker.perform_async(user.id)
+      Users::Bittrex::SyncOrdersForUserWorker.perform_async(current_user.id)
     else
-      flash[:error] = user.errors.full_messages.join(', ')
+      flash[:error] = current_user.errors.full_messages.join(', ')
     end
 
     redirect_back fallback_location: root_path
