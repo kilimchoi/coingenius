965ec26a0cc35bfbb69813828ebf22033628c69c
Timur Khafizov <hangmany@gmail.com>
2017-09-20 21:49:28 +0200

    Fix parameters sanitizing for Bittrex integration

--- app/controllers/bittrex_integrations_controller.rb
+++ app/controllers/bittrex_integrations_controller.rb
@@ -1,21 +1,26 @@
 class BittrexIntegrationsController < ApplicationController
   respond_to :html
-  expose :user, -> { current_user }
+
+  before_action :strip_whitespaces_in_params!, only: :create
+
+  expose :user, -> {current_user}
 
   def new
   end
 
   def create
-    accounts_with_same_api_key = User.where(bittrex_api_key: params["user"]["bittrex_api_key"])
-    strip_whitespace(*params[:user])
-    if !accounts_with_same_api_key.empty?
+    accounts_with_same_api_key = User.where(bittrex_api_key: bittrex_params[:bittrex_api_key])
+
+    if accounts_with_same_api_key.any?
       flash[:error] = "You have already imported transactions from your Bittrex account using another CoinGenius account. Please use a different Bittrex account and create a new key."
-    elsif user.update_attributes(bittrex_params)
+    elsif user.update(bittrex_params)
       flash[:success] = "You successfully integrated with bittrex. We will be pulling transactions from bittrex."
+
       Users::Bittrex::SyncOrdersForUserWorker.perform_async(user.id)
     else
       flash[:error] = user.errors.full_messages.join(', ')
     end
+
     redirect_back fallback_location: root_path
   end
 
@@ -27,7 +32,7 @@ class BittrexIntegrationsController < ApplicationController
 
   private
 
-  def strip_whitespace(*params)
-    params.map{ |attr| attr.last.strip! }
+  def strip_whitespaces_in_params!
+    bittrex_params.to_h.map {|_key, value| value&.strip!}
   end
 end
