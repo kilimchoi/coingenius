7981fce7d7160c43f8f3274dfa5a7e922077a905
kilim choi <stevn1202@gmail.com>
2017-09-23 09:38:34 -0700

    create abstract transaction class and let others inherit from it

--- app/models/coinbase/deposit.rb
+++ app/models/coinbase/deposit.rb
@@ -1,9 +1,5 @@
 module Coinbase
-  class Deposit < ActiveRecord::Base
+  class Deposit < Coinbase::Transaction
     self.table_name = "coinbase_deposits"
-
-    belongs_to :user_transaction, class_name: "Transaction", foreign_key: :transaction_id
-
-    validates :uuid, presence: true
   end
 end
