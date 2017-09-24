ef21c718d54f1bc741cfea08915509d2c0fa741b
kilim choi <stevn1202@gmail.com>
2017-09-23 22:53:04 -0700

    add ui for importing bittrex csv file

--- app/controllers/bittrex_integrations_controller.rb
+++ app/controllers/bittrex_integrations_controller.rb
@@ -2,11 +2,21 @@ class BittrexIntegrationsController < ApplicationController
   respond_to :html
 
   before_action :strip_whitespaces_in_params!, only: :create
+  expose :bittrex_orders_history_import
 
   def new
   end
 
   def create
+    if !params["bittrex_orders_history_import_params"].nil?
+      bittrex_orders_history_import = current_user.bittrex_orders_history_imports.create!(
+        file_content: bittrex_orders_history_import_params[:history_file].read
+      )
+      BittrexOrdersHistoryImports::ProcessWorker.perform_async(bittrex_orders_history_import.id)
+
+      redirect_to portfolio_root_path, notice: "Your Bittrex orders history file will be processed soon"
+    end
+
     accounts_with_same_api_key = User.where(bittrex_api_key: bittrex_params[:bittrex_api_key]).where.not(id: current_user.id)
 
     if accounts_with_same_api_key.any?
@@ -26,6 +36,10 @@ class BittrexIntegrationsController < ApplicationController
 
   def bittrex_params
     params.require(:user).permit(:bittrex_api_key, :bittrex_api_secret)
+  end
+
+  def bittrex_orders_history_import_params
+    params.require(:bittrex_orders_history_import).permit(:history_file)
   end
 
   private
