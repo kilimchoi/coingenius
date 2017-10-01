3b46f81a9d23c0188f44c35b798e6f9ba5209c11
Timur Khafizov <hangmany@gmail.com>
2017-10-01 16:47:40 +0200

    Remove useless `edit` reoute for transactions in API V1

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :login, only: :create, controller: :login
-      resources :transactions
+      resources :transactions, except: :edit
     end
   end
 
