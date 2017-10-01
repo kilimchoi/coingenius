157431029e849c9b63f8569fabcf5e44ba0125e6
Timur Khafizov <hangmany@gmail.com>
2017-10-01 15:04:00 +0200

    Skip API auth on dev

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class ApplicationController < ActionController::API
       include Api::Authentication::SignedRequest
+      include Api::Authentication::SignedRequest::SkipOnDev
     end
   end
 end
