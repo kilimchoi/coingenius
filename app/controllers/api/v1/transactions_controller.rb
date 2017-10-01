303587216d2af0af011faa939714f9d79460d8f2
Timur Khafizov <hangmany@gmail.com>
2017-10-01 16:18:10 +0200

    Add transactions API v1

---
+++ app/controllers/api/v1/transactions_controller.rb
@@ -0,0 +1,42 @@
+module Api
+  module V1
+    class TransactionsController < Api::V1::ApplicationController
+      respond_to :json
+
+      expose :transaction, scope: -> { transactions }
+      expose :transactions, -> { current_user.transactions }
+
+      def show
+        respond_with transaction
+      end
+
+      def index
+        respond_with transactions
+      end
+
+      def create
+        transaction.save
+
+        respond_with transaction, location: api_v1_transaction_url(transaction.id)
+      end
+
+      def update
+        transaction.update(transaction_params)
+
+        respond_with transaction, location: api_v1_transaction_url(transaction.id)
+      end
+
+      def destroy
+        transaction.destroy
+      end
+
+      private
+
+      def transaction_params
+        params
+          .require(:transaction)
+          .permit(:amount, :btc_price, :coin_id, :is_expired, :price, :transaction_date, :transaction_type)
+      end
+    end
+  end
+end
---
+++ app/views/api/v1/transactions/create.json.jbuilder
@@ -0,0 +1,2 @@
+json.partial! "api/v1/transactions/transaction",
+              transaction: transaction
