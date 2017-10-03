e6d2dd84e50d3dec2c5573e876e9c0eb682f84a3
kilim choi <stevn1202@gmail.com>
2017-10-02 20:44:32 -0700

    double quotes

--- app/models/coinbase/transaction.rb
+++ app/models/coinbase/transaction.rb
@@ -1,8 +1,8 @@
 module Coinbase
   class Transaction < ApplicationRecord
     self.abstract_class = true
-    self.table_name = 'transactions'
-    
+    self.table_name = "transactions"
+
     belongs_to :user_transaction, class_name: "Transaction", foreign_key: :transaction_id
 
     validates :uuid, presence: true
