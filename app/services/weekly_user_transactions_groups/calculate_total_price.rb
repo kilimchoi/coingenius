ad584608f63b404f1ae6bb4ff4713f4d42fe146a
Emil Shakirov <me+github@emil.sh>
2017-11-02 12:07:44 +0300

    fix WeeklyUserTransactionsGroups::CalculateTotalPrice (#85)

--- app/services/weekly_user_transactions_groups/calculate_total_price.rb
+++ app/services/weekly_user_transactions_groups/calculate_total_price.rb
@@ -9,11 +9,13 @@ module WeeklyUserTransactionsGroups
 
     before do
       today_timestamp = Time.zone.now.beginning_of_day.to_i
-      context.current_price = Coins::GetCachedPriceHistory.call(
+      prices = Coins::GetCachedPriceHistory.call(
         coin: coin,
         days: 1,
         price_currency: DEFAULT_CURRENCY
-      )[today_timestamp].to_f
+      ).results
+
+      context.current_price = prices[today_timestamp].to_f
     end
 
     def call
