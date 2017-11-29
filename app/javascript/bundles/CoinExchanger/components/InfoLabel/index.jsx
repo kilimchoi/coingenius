50b2afc0c96fcad27372002d67d8e377d572fd37
Emil Shakirov <me+github@emil.sh>
2017-11-29 12:21:59 +0300

    Fix InfoLabel component (#102)

--- app/javascript/bundles/CoinExchanger/components/InfoLabel/index.jsx
+++ app/javascript/bundles/CoinExchanger/components/InfoLabel/index.jsx
@@ -1,17 +1,32 @@
-import React, { Component } from "react";
-import { UncontrolledTooltip } from "reactstrap";
-import FontAwesome from "react-fontawesome";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { UncontrolledTooltip } from 'reactstrap';
+import FontAwesome from 'react-fontawesome';
 
 class InfoLabel extends Component {
   render() {
     const { children, iconId, tooltipText } = this.props;
-    return <h5>
+    return (
+      <h5>
         {children} <FontAwesome name="info-circle" id={iconId} />
-        <UncontrolledTooltip placement="right" target={iconId}>
-          {tooltipText}
-        </UncontrolledTooltip>
-      </h5>;
+        {iconId && (
+          <UncontrolledTooltip placement="right" target={iconId}>
+            {tooltipText}
+          </UncontrolledTooltip>
+        )}
+      </h5>
+    );
   }
 }
+
+InfoLabel.propTypes = {
+  iconId: PropTypes.string,
+  tooltipText: PropTypes.string,
+};
+
+InfoLabel.defaultProps = {
+  iconId: '',
+  tooltipText: '',
+};
 
 export default InfoLabel;
