aa6ef3327d1b715c012bebe762a07e47f5bd1cf5
Emil Shakirov <me+github@emil.sh>
2017-12-01 11:37:00 +0300

    Fix rate fetching (#103)

--- app/javascript/bundles/CoinExchanger/components/StepOne/index.jsx
+++ app/javascript/bundles/CoinExchanger/components/StepOne/index.jsx
@@ -1,48 +1,23 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  InputGroup,
-  InputGroupAddon
-} from "reactstrap";
-import { decamelizeKeys } from "humps";
-import { Navigation } from "react-albus";
-import CurrencyInput from "_bundles/CoinExchanger/components/CurrencyInput";
-import InfoLabel from "_bundles/CoinExchanger/components/InfoLabel";
-import propTypes from "_bundles/CoinExchanger/propTypes";
-import { buildConversion } from "_sources/convertions";
-import { disabledInput } from "./styles.css";
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
+import PropTypes from 'prop-types';
+import { Button, Form, FormGroup, Label, Input, InputGroup, InputGroupAddon } from 'reactstrap';
+import { Navigation } from 'react-albus';
+import CurrencyInput from '_bundles/CoinExchanger/components/CurrencyInput';
+import InfoLabel from '_bundles/CoinExchanger/components/InfoLabel';
+import propTypes from '_bundles/CoinExchanger/propTypes';
+import { disabledInput } from './styles.css';
 
 class StepOne extends Component {
   constructor(props) {
     super(props);
 
-    this.fetchRate();
+    this.sendAmountInput = null;
   }
 
   handleCurrencyChange = (name, value) => {
     if (value && this.props[name] !== value) {
       this.props.onValueChange(name, value);
-      this.fetchRate();
-    }
-  };
-
-  fetchRate = () => {
-    const { onValueChange, sendingCoin, receiveCoin } = this.props;
-
-    if (sendingCoin && receiveCoin && sendingCoin.id && receiveCoin.id) {
-      const params = decamelizeKeys({
-        receiveCoinId: receiveCoin.id,
-        sendingCoinId: sendingCoin.id
-      });
-
-      buildConversion(params).then(({ serializedBody: { rate } }) => {
-        onValueChange("rate", rate);
-      });
     }
   };
 
@@ -52,27 +27,29 @@ class StepOne extends Component {
     return sendingCoin && receiveCoin;
   };
 
-  isNextEnabled = () => {
+  areCoinsPresent = () => {
     const { sendAmount, sendingCoin, receiveCoin } = this.props;
 
-    return !(
-      sendAmount &&
-      sendingCoin &&
-      receiveCoin &&
-      sendingCoin.id &&
-      receiveCoin.id
-    );
+    return sendAmount && sendingCoin && receiveCoin && sendingCoin.id && receiveCoin.id;
   };
+
+  isSendInputValid = () => this.sendAmountInput && this.sendAmountInput.checkValidity();
+
+  isNextDisabled = () => !this.areCoinsPresent() || !this.isSendInputValid();
 
   render() {
     const {
       coins,
+      maxAmount,
+      minAmount,
       onValueChange,
       sendAmount,
       sendingCoin,
       receiveCoin,
-      receiveAmount
+      receiveAmount,
     } = this.props;
+
+    const inputConstraints = { max: maxAmount, min: minAmount };
 
     return (
       <div>
@@ -86,23 +63,28 @@ class StepOne extends Component {
                 You send
               </InfoLabel>
             </Label>
+            <div className="pull-right">
+              Min {minAmount}
+              {' | '}
+              Max {maxAmount}
+            </div>
             <InputGroup>
               <Input
+                {...inputConstraints}
                 name="sendAmount"
                 type="number"
-                step="0.01"
+                step="0.0001"
                 value={sendAmount}
-                onChange={event =>
-                  onValueChange("sendAmount", event.target.value)
-                }
+                ref={(input) => {
+                  this.sendAmountInput = findDOMNode(input);
+                }}
+                onChange={event => onValueChange('sendAmount', event.target.value)}
               />
               <InputGroupAddon>
                 <CurrencyInput
                   value={sendingCoin}
                   options={coins}
-                  onChange={([currency]) =>
-                    this.handleCurrencyChange("sendingCoin", currency)
-                  }
+                  onChange={([currency]) => this.handleCurrencyChange('sendingCoin', currency)}
                 />
               </InputGroupAddon>
             </InputGroup>
@@ -124,17 +106,13 @@ class StepOne extends Component {
                 type="number"
                 step="0.01"
                 value={receiveAmount}
-                onChange={event =>
-                  onValueChange("receiveAmount", event.target.value)
-                }
+                onChange={event => onValueChange('receiveAmount', event.target.value)}
               />
               <InputGroupAddon>
                 <CurrencyInput
                   value={receiveCoin}
                   options={coins}
-                  onChange={([currency]) =>
-                    this.handleCurrencyChange("receiveCoin", currency)
-                  }
+                  onChange={([currency]) => this.handleCurrencyChange('receiveCoin', currency)}
                 />
               </InputGroupAddon>
             </InputGroup>
@@ -143,12 +121,7 @@ class StepOne extends Component {
         <Navigation
           render={({ next }) => (
             <div>
-              <Button
-                className="w-100"
-                disabled={this.isNextEnabled()}
-                size="lg"
-                onClick={next}
-              >
+              <Button className="w-100" disabled={this.isNextDisabled()} size="lg" onClick={next}>
                 Next
               </Button>
             </div>
@@ -162,7 +135,7 @@ class StepOne extends Component {
 StepOne.propTypes = {
   onValueChange: PropTypes.func.isRequired,
   coins: PropTypes.array.isRequired,
-  ...propTypes
+  ...propTypes,
 };
 
 export default StepOne;
