661b241d65397d9911bd01a5f3cff303cdc9e827
Timur Khafizov <hangmany@gmail.com>
2017-12-09 21:41:18 +0100

    Override `fixed_amount_transaction` instead of `shift`

--- config/initializers/01_shapeshift_override.rb
+++ config/initializers/01_shapeshift_override.rb
@@ -1,21 +1,23 @@
 ShapeShiftRuby::Client.class_eval do
-  def shift(
-    withdrawal:,
-    pair:,
-    return_address: "",
-    dest_tag: "",
-    rs_address: "",
-    api_key: "",
-    payment_id: nil
-  )
-    api_request(url: url("shift"), method: "post", params: {
-                  pair: pair,
-                  withdrawal: withdrawal,
-                  returnAddress: return_address,
-                  destTag: dest_tag,
-                  rsAddress: rs_address,
-                  paymentId: payment_id,
-                  apiKey: api_key
-                })
+  def fixed_amount_transaction(amount:,
+                               api_key: "",
+                               dest_tag: "",
+                               pair:,
+                               payment_id: "",
+                               return_address: "",
+                               rs_address: "",
+                               withdrawal:)
+    params = {
+      amount: amount,
+      apiKey: api_key,
+      destTag: dest_tag,
+      pair: pair,
+      paymentId: payment_id,
+      returnAddress: return_address,
+      rsAddress: rs_address,
+      withdrawal: withdrawal
+    }
+
+    api_request(url: url("sendamount"), method: "post", params: params)
   end
 end
