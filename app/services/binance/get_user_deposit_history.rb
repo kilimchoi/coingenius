b0a3be15fb480d6c83473e6412cb2d361a07f5ae
Timur Khafizov <hangmany@gmail.com>
2018-01-06 12:41:48 +0100

    Add `Binance::GetUserDepositHistory` service object

---
+++ app/services/binance/get_user_deposit_history.rb
@@ -0,0 +1,17 @@
+module Binance
+  class GetUserDepositHistory
+    include Interactor
+
+    delegate :user, to: :context
+
+    def call
+      context.deposit_history = DepositHistory.new(client.deposit_history["depositList"])
+    end
+
+    private
+
+    def client
+      @client ||= Binance::Client::REST.new(api_key: user.binance_api_key, secret_key: user.binance_api_secret)
+    end
+  end
+end
