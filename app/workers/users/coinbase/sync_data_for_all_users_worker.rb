46de0292b622d9a0aa49bf22ea121f145d152861
kilim choi <stevn1202@gmail.com>
2018-01-10 18:43:06 +0900

    wrap worker under one task

---
+++ app/workers/users/coinbase/sync_data_for_all_users_worker.rb
@@ -0,0 +1,20 @@
+module Users
+  module Coinbase
+    class SyncDataForAllUsersWorker
+      include Sidekiq::Worker
+      
+      sidekiq_options retry: 5
+      
+      def perform
+        User.find_each do |user|
+          Users::Coinbase::SyncBuysForUserWorker.perform_async(user.id)
+          Users::Coinbase::SyncSellsForUserWorker.perform_async(user.id)
+          Users::Coinbase::SyncDepositsForUserWorker.perform_async(user.id)
+          Users::Coinbase::SyncWithdrawalsForUserWorker.perform_async(user.id)
+          Users::Coinbase::SyncReceivedForUserWorker.perform_async(user.id)
+          Users::Coinbase::SyncSentForUserWorker.perform_async(user.id)
+        end
+      end
+    end
+  end
+end
