0a3c00c14f65662f12553e66b97a44eb1eb2fbe3
Timur Khafizov <hangmany@gmail.com>
2018-01-10 07:16:33 +0100

    Add `Users::Binance::SyncDataWorker`

---
+++ app/workers/users/binance/sync_data_worker.rb
@@ -0,0 +1,15 @@
+module Users
+  module Binance
+    class SyncDataWorker
+      include Sidekiq::Worker
+
+      sidekiq_options retry: 5
+
+      def perform(user_id)
+        Users::Binance::SyncDepositsWorker.perform_async(user_id)
+        Users::Binance::SyncWithdrawalsWorker.perform_async(user_id)
+        Users::Binance::SyncOrdersWorker.perform_async(user_id)
+      end
+    end
+  end
+end
