8ceb800e6db7ae2e30eaef18e26a1452927d7dc6
Timur Khafizov <hangmany@gmail.com>
2018-01-10 07:20:37 +0100

    Create wrapper class `Users::Binance::SyncDataForAllUsersWorker`

---
+++ app/workers/users/binance/sync_data_for_all_users_worker.rb
@@ -0,0 +1,18 @@
+module Users
+  module Binance
+    # This is just a wrapper class to be able to perform the full Binance sync for all users
+    class SyncDataForAllUsersWorker
+      include Sidekiq::Worker
+
+      sidekiq_options retry: 5
+
+      def perform
+        User.find_each do |user|
+          Users::Binance::SyncDepositsWorker.perform_async(user.id)
+          Users::Binance::SyncWithdrawalsWorker.perform_async(user.id)
+          Users::Binance::SyncOrdersWorker.perform_async(user.id)
+        end
+      end
+    end
+  end
+end
