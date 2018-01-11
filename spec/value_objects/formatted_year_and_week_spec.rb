84bd3643b86cea19ab662c6c58f6ca299f9f6015
Emil Shakirov <me+github@emil.sh>
2018-01-11 22:36:58 +0100

    Fix year formatting for beginning and end of the year (#111)

--- spec/value_objects/formatted_year_and_week_spec.rb
+++ spec/value_objects/formatted_year_and_week_spec.rb
@@ -9,5 +9,25 @@ describe FormattedYearAndWeek do
       expect(value_object.next).to eq("2001-10")
       expect(value_object.previous).to eq("2001-08")
     end
+
+    context "when handling start of the year" do
+      let(:value_object) { described_class.new("01-01-2001".to_time) }
+
+      it "returns formatted year and date" do
+        expect(value_object.value).to eq("2001-01")
+        expect(value_object.next).to eq("2001-02")
+        expect(value_object.previous).to eq("2000-52")
+      end
+    end
+
+    context "when handling end of the year" do
+      let(:value_object) { described_class.new("31-12-2001".to_time) }
+
+      it "returns formatted year and date" do
+        expect(value_object.value).to eq("2002-01")
+        expect(value_object.next).to eq("2002-02")
+        expect(value_object.previous).to eq("2001-52")
+      end
+    end
   end
 end
