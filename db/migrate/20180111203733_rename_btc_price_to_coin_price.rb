e92a1418efa5162a384348465f447c1d40cd3504
Timur Khafizov <hangmany@gmail.com>
2018-01-11 22:51:40 +0100

    Rename `btc_price` to `converted_coin_price`, add `converted_coin_id`, and `linked_transaction_id`

--- db/migrate/20180111203733_rename_btc_price_to_coin_price.rb
+++ db/migrate/20180111203733_rename_btc_price_to_coin_price.rb
@@ -2,9 +2,13 @@ class RenameBtcPriceToCoinPrice < ActiveRecord::Migration[5.1]
   def change
     add_column    :transactions, :converted_coin_id, :bigint
     rename_column :transactions, :btc_price, :converted_coin_price
+    add_column    :transactions, :linked_transaction_id, :bigint
 
     add_index :transactions, :converted_coin_id
     add_foreign_key :transactions, :coins, column: :converted_coin_id
+
+    add_index :transactions, :linked_transaction_id
+    add_foreign_key :transactions, :transactions, column: :linked_transaction_id
 
     reversible do |dir|
       dir.up do
