3cd119fea4029d28f4359492f74f874af046114d
Timur Khafizov <hangmany@gmail.com>
2018-01-13 22:00:42 +0100

    Auto-correct code quality issues by `rubocop`

--- spec/factories/binance_deposits.rb
+++ spec/factories/binance_deposits.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :binance_deposit, class: 'Binance::Deposit' do
+  factory :binance_deposit, class: "Binance::Deposit" do
     amount "9.99"
     raw_data ""
     executed_at "2018-01-07 17:36:45"
